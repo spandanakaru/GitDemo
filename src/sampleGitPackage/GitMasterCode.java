502c48ccd4562144732e77776dfcf8263b8d07cf
Spandana  Karu <spandana.k@avowstech.com>
2024-09-16 10:04:19 +0530

    first commit

---
+++ src/sampleGitPackage/GitMasterCode.java
@@ -0,0 +1,10 @@
+package sampleGitPackage;
+
+public class GitMasterCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this test is from git master project");
+	}
+
+}
