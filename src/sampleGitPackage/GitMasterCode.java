5fde6ded2ca329f37cc6e5a4519c5db7c91851a6
Spandana  Karu <spandana.k@avowstech.com>
2024-09-16 10:21:14 +0530

    this is changed after 1st clone

--- src/sampleGitPackage/GitMasterCode.java
+++ src/sampleGitPackage/GitMasterCode.java
@@ -5,6 +5,7 @@ public class GitMasterCode {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this test is from git master project");
+		System.out.println("This line is added by second guy by cloning master project");
 	}
 
 }
