6adcfc3d96cc9e3ec7a9f8538455f91ef2669b4e
Spandana  Karu <spandana.k@avowstech.com>
2024-09-16 11:38:59 +0530

    this is from another guy to develop branch

--- src/sampleGitPackage/GitNewBranch.java
+++ src/sampleGitPackage/GitNewBranch.java
@@ -5,6 +5,8 @@ public class GitNewBranch {
 	public static void main(String[] args)
 	{
 		System.out.println("created new branch and adding code from it");
+		
+		System.out.println("adding code again");
 	}
 
 }
