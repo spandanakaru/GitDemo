e6150c682987796ec378c9bc9ea52c61eb794e35
Spandana  Karu <spandana.k@avowstech.com>
2024-09-16 11:54:02 +0530

    final commit

---
+++ src/sampleGitPackage/GitTestMerge.java
@@ -0,0 +1,10 @@
+package sampleGitPackage;
+
+public class GitTestMerge {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("merge test");
+	}
+
+}
